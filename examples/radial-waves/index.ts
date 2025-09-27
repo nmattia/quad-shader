a58a04b62900f2989546022e2e9f965a9cea9b3f
Nicolas Mattia <nicolas@nmattia.com>
2025-09-27 18:08:47 +0200

    Add codeblocks

--- examples/radial-waves/index.ts
+++ examples/radial-waves/index.ts
@@ -1,21 +1,12 @@
 import { animate, getComputedStylePropRGBA } from "quad-shader";
+import frag from "./frag.glsl?raw" // PROC: REMOVE
 const canvas = document.querySelector("#glcanvas-radial-waves") as HTMLCanvasElement; // PROC: REMOVE
 const qs = animate(
     canvas, /* the HTMLCanvasElement to draw on */
-    `
-    precision lowp float;
-    varying vec2 vPosition;
-    uniform vec4 uColor;
-    uniform float uTime;
-    void main() {
-        float theta = atan(vPosition.y, vPosition.x);
-        float rho = length(vPosition.xy);
-        float v = mod(rho - uTime/10., .2);
-        float alpha = smoothstep(.1, .2, v);
-        alpha *= (1. - smoothstep(0., 1., rho));
-        float fadeIn = smoothstep(0., 1., uTime);
-        gl_FragColor = fadeIn * alpha * uColor;
-    }
-    `,
+    frag, /* fragment shader, as a string */
 );
+
+/* register a callback that updates "uColor" on render */
+/* 'getComputedStylePropRGBA' is a helper that returns the */
+/*  given CSS property as a RGBA value */
 qs.uniform4f("uColor", () => getComputedStylePropRGBA(canvas, "accent-color")); // PROC: REPLACE "accent-color" "color"
